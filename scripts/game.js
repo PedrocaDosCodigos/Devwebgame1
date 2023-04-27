b7c866bd02af0824dc35eb36cba5750c776e57cf
Victoria-Tadei-Carvalho <victoriatadeicarvalho@gmail.com>
2023-04-27 17:21:56 -0300

    bug arrumado

--- scripts/game.js
+++ scripts/game.js
@@ -4,19 +4,22 @@ function loadgame() {
     board.push("start")
 
     for (let i = 2; i <= 24; i++) {
-        console.log(i)
+        console.log(i);
         const randomPath = Math.floor(Math.random() * 2)
         
         board.push(randomPath)
         
         const element = document.getElementById("path" + i)
-        
+       if(i===15){
+        console.log(element);
+       } 
         if (randomPath === 0) {
             element.style.backgroundImage = "url('./assets/tiles/path_free.png')"
         } else {  
             element.style.backgroundImage = "url('./assets/tiles/path_blocked.png')"
 
         }
+     
     }
 
 
