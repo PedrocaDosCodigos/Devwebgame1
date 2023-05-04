3ef1b3688435bf30cdfd1e770c8bf27e2bf03847
Victoria-Tadei-Carvalho <victoriatadeicarvalho@gmail.com>
2023-05-04 17:05:43 -0300

    mais div

--- scripts/game.js
+++ scripts/game.js
@@ -3,23 +3,22 @@ const board = []
 function loadgame() {
     board.push("start")
 
-    for (let i = 2; i <= 24; i++) {
+    for (let i = 2; i <= 35; i++) {
+        
         console.log(i);
         const randomPath = Math.floor(Math.random() * 2)
-        
+
         board.push(randomPath)
-        
+
         const element = document.getElementById("path" + i)
-       if(i===15){
-        console.log(element);
-       } 
+  
         if (randomPath === 0) {
             element.style.backgroundImage = "url('./assets/tiles/path_free.png')"
-        } else {  
-            element.style.backgroundImage = "url('./assets/tiles/path_blocked.png')"
+        } else {
+            element.style.backgroundImage = "url('./assets/tiles/arbusto.png')"
 
         }
-     
+
     }
 
 
