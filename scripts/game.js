cc99e84f3bc0eb5e3cb8ff81736cfbacfea67898
Victoria-Tadei-Carvalho <victoriatadeicarvalho@gmail.com>
2023-05-11 17:32:07 -0300

    uns negoço

--- scripts/game.js
+++ scripts/game.js
@@ -4,14 +4,14 @@ function loadgame() {
     board.push("start")
 
     for (let i = 2; i <= 99; i++) {
-        
+
         console.log(i);
         const randomPath = Math.floor(Math.random() * 2)
 
         board.push(randomPath)
 
         const element = document.getElementById("path" + i)
-  
+
         if (randomPath === 0) {
             element.style.backgroundImage = "url('./assets/tiles/path_free.png')"
         } else {
@@ -25,7 +25,10 @@ function loadgame() {
     console.log(board)
 }
 loadgame();
-
+function dialogue() {
+    const dialogueButton = document.getElementById("btn-dialogue")
+dialogueButton.style.display="none"
+}
 
 
 
